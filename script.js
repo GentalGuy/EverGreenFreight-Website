ea4bcbdd5e2aa22bca5994eac09a40fb6f1a7b93
GentalGuy <gurkirat836.w@gmail.com>
2024-06-14 15:35:37 -0400

    header hover change and some javascript fix

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
 var tl = gsap.timeline();
-tl.from("#nav1 .header-left img", {
-  y: -50,
-  opacity: 0,
-  delay: 0,
-  duration: 0.8,
-});
-tl.from("#nav1 .header-left h1", {
-  y: -50,
-  opacity: 0,
-  delay: 0,
-  duration: 0.8,
-});
 
-tl.from("#nav1 .nav1elem", {
-  y: -50,
-  opacity: 0,
-  delay: 0,
-  duration: 0.5,
-  stagger: 0.3,
-});
 
-gsap.from(".menubtn", {
-  x: 50,
-  delay: 2,
-  opacity: 0,
-  duration: 0.8,
-});
-
-gsap.from(".headSec01 span", {
+tl.from(".headSec01 span", {
   opacity: 0,
   delay: 1,
   duration: 1,
@@ -40,29 +14,21 @@ tl.from(".sec01anim", {
   duration: 0.8,
 });
 
-let headerleft = document.querySelector(".header-left");
-let navElem = document.querySelector(".hoverHead");
-let nav1 = document.querySelector("#nav1");
 
-let menubtn = document.querySelector(".menubtn");
-let menuInside = document.querySelector(".menu-inside");
-let closebtn = document.querySelector(".closebtn");
-let header = document.querySelector("header");
+const menuButton = document.querySelector("#menu-button");
+const rootElement = document.documentElement;
 
-const toggleNavbar = () => {
-  header.classList.toggle("active");
-};
-
-closebtn.addEventListener("click", function () {
-  header.classList.toggle("active");
+menuButton.addEventListener("click", () => {
+  rootElement.toggleAttribute("menu-open");
+  
 });
 
-menuInside.addEventListener("click", function () {
-  header.classList.remove("active");
+const menuel = document.querySelectorAll('.nav2el');
+menuel.forEach(el => {
+  el.addEventListener('click', function() {
+      rootElement.toggleAttribute('menu-open');
+  });
 });
-
-menubtn.addEventListener("click", () => toggleNavbar());
-
 
 const loaderContainer = document.querySelector('.loader-container');
 const pageContent = document.querySelector('#page-content');
